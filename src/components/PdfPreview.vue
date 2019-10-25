37bd503b13eb7e78a67b4092813e1778f3ada44c
Lushenggang <lushg-tcxg@qq.com>
2019-10-26 03:31:39 +0800

    Optimization

--- src/components/PdfPreview.vue
+++ src/components/PdfPreview.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="container">
+  <div class="pdf-preview-container">
     <div
-      v-for="page in pages"
-      :key="page"
-      ref="pageContainer"
+      class="page-container"
+      ref="container"
+      v-for="page in docPages"
       :style="{
-        borderBottom: '1px solid #DFDFDF',
-        height: pageHeight + 'px'
-      }">
-      <canvas v-if="renderList.includes(page)"></canvas>
+        height: `${pageHeight}px`
+      }"
+      :key="page">
+      <canvas v-if="renderList.includes(page)">
+      </canvas>
+    </div>
+    <div class="notice" v-if="show">
+      {{ notice }}
     </div>
   </div>
 </template>
@@ -22,52 +26,56 @@ export default {
       type: String,
       required: true
     },
-    manualRender: {
-      type: Boolean,
-      default: false
-    },
-    renderPages: { // 前后各预渲染多少页
+    renderPages: {
       type: Number,
       default: 5
     },
-    showPageLine: {
+    customScroll: {
       type: Boolean,
-      default: true
+      default: false
+    },
+    offsetHeight: {
+      type: Number,
+      default: 0
     }
   },
   data () {
     return {
-      pdfDoc: null,
-      pages: 0,
+      doc: null,
+      docPages: 0,
       currentPage: 0,
       pageHeight: 0,
-      renderList: [], // 渲染的页码数组
-      timerIndex: 0
+      renderList: [],
+      show: false,
+      notice: '',
+      timer: 0
     }
   },
   watch: {
-    url () {
-      this.getPDFFile()
+    url: {
+      immediate: true,
+      handler () {
+        this.getPDFFile()
+      }
     }
   },
   created () {
-    this.getPDFFile()
-    if (!this.manualRender) {
-      document.addEventListener('scroll', this.documentScroll)
+    if (!this.customScroll) {
+      document.addEventListener('scroll', this.scroll)
     }
   },
   beforeDestroy () {
-    document.removeEventListener('scroll', this.documentScroll)
-    this.timerIndex && clearTimeout(this.timerIndex)
+    document.removeEventListener('scroll', this.scroll)
   },
   methods: {
     getPDFFile () {
       if (!this.url) return
+      this.currentPage = 0
       pdfJS.getDocument(this.url).then(pdf => {
-        this.pdfDoc = pdf
-        this.pages = this.pdfDoc.numPages
+        this.doc = pdf
+        this.docPages = pdf.numPages
         this.$nextTick(() => {
-          this.pages && this.scrollToPage(1)
+          this.docPages && this.scrollToPage(1)
         })
       })
     },
@@ -78,9 +86,7 @@ export default {
       for (let page = pageNo - this.renderPages; page <= pageNo + this.renderPages; page++) {
         list.push(page)
       }
-      for (let idx = list.length; idx >= 0; idx--) {
-        if (list[idx] > this.pages || list[idx] < 1) list.splice(idx, 1)
-      }
+      list = list.filter(page => page <= this.docPages && page >= 1)
       this.$nextTick(() => {
         this.renderList = list
         this.renderList.forEach(page => {
@@ -88,53 +94,58 @@ export default {
         })
       })
     },
+    // 渲染page
     renderPage (pageNo) {
-      this.pdfDoc.getPage(pageNo).then(page => {
-        let containerNode = this.$refs.pageContainer[pageNo - 1]
-        if (!containerNode) return
-        let canvas = containerNode.querySelector('canvas')
+      this.doc.getPage(pageNo).then(page => {
+        let container = this.$refs.container[pageNo - 1]
+        if (!container) return
+        let canvas = container.querySelector('canvas')
         if (!canvas || canvas.__rendered) return
         let ctx = canvas.getContext('2d')
         let dpr = window.devicePixelRatio || 1
         let bsr = ctx.webkitBackingStorePixelRatio || ctx.mozBackingStorePixelRatio || ctx.msBackingStorePixelRatio || ctx.oBackingStorePixelRatio || ctx.backingStorePixelRatio || 1
         let ratio = dpr / bsr
-        let viewport = page.getViewport(screen.availWidth / page.getViewport(.5).width)
-        canvas.width = viewport.width * ratio
-        canvas.height = viewport.height * ratio
-        canvas.style.width = viewport.width + 'px'
-        canvas.style.height = viewport.height + 'px'
-        this.pageHeight = viewport.height
+        let rect = container.getBoundingClientRect()
+        let viewport = page.getViewport(1)
+        let width = rect.width
+        let height = width / viewport.width * viewport.height
+        canvas.style.width = `${width}px`
+        canvas.style.height = `${height}px`
+        this.pageHeight = height
+        canvas.height = height * ratio
+        canvas.width = width * ratio
         ctx.setTransform(ratio, 0, 0, ratio, 0, 0)
-        let renderCtx = {
+        page.render({
           canvasContext: ctx,
-          viewport
-        }
-        page.render(renderCtx)
+          viewport: page.getViewport(width/viewport.width)
+        })
         canvas.__rendered = true
       })
     },
-    documentScroll () {
-      this.checkRender()
+    scroll () {
+      this.checkRender(document.documentElement)
     },
-    checkRender () {
-      this.timerIndex && clearTimeout(this.timerIndex)
-      this.timerIndex = setTimeout(this.renderCurrent, 100)
-    },
-    renderCurrent () {
-      this.$nextTick(() => {
-        if (this.pages <= 0) return
-        if (!this.pageHeight) return
-        if (!this.$refs.pageContainer[0]) return
-        let rect = this.$refs.pageContainer[0].getBoundingClientRect()
-        if (rect.top >= 0) {
-          this.scrollToPage(1)
-        } else {
-          let page = Math.floor(-rect.top / this.pageHeight) + 1
-          if (page > this.pages) page = this.pages
-          this.scrollToPage(page)
-        }
-      })
+    checkRender (el) {
+      if (!this.pageHeight) return
+      let scrollTop = el.scrollTop
+      if (el === document.documentElement) {
+        scrollTop = el.scrollTop || window.pageYOffset || document.body.scrollTop
+      }
+      let page = Math.floor((scrollTop - this.offsetHeight) / this.pageHeight)
+      page = Math.max(page, 1)
+      page = Math.min(page, this.docPages)
+      this.scrollToPage(page)
     }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.notice
+  position fixed
+  background #000
+  color #fff
+  left 2rem
+  top 5rem
+  right 2rem
+</style>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import pdfJS from 'pdfjs-dist'
 import pdfPreview from './components/PdfPreview.vue'
 
 export default pdfPreview
